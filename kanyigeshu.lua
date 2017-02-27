657a4db66e33c3e67c731e8e983149521fe18a57
lanlanren1 <382722106@qq.com>
2017-02-27 08:21:42 +0800

    cut 1*1 trees.

---
+++ kanyigeshu.lua
@@ -0,0 +1,23 @@
+s=1
+term.clear()
+sucess,biaoji=turtle.inspect()	
+-- turtle.dig()
+-- turtle.forward()
+print("biaoji:",biaoji.name)
+-- while true do
+	-- s,muzhuang=turtle.inspectUp()
+	-- if muzhuang.name==biaoji.name then
+		-- turtle.digUp()
+		-- turtle.up()
+		-- s=s+1
+	-- else
+		-- break
+	-- end
+-- end
+-- while not turtle.detectDown() do
+	-- turtle.down()
+-- end
+print("high= ",s)
+
+  
+  
