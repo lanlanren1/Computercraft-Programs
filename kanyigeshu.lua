5f4749843b3b6fa1e889beb56129a7ac6649188b
lanlanren1 <382722106@qq.com>
2017-02-27 08:54:09 +0800

    第一个版本，工作正常。

--- kanyigeshu.lua
+++ kanyigeshu.lua
@@ -1,22 +1,23 @@
 s=1
 term.clear()
+term.setCursorPos(1,1)
 sucess,biaoji=turtle.inspect()	
--- turtle.dig()
--- turtle.forward()
+turtle.dig()
+turtle.forward()
 print("biaoji:",biaoji.name)
--- while true do
-	-- s,muzhuang=turtle.inspectUp()
-	-- if muzhuang.name==biaoji.name then
-		-- turtle.digUp()
-		-- turtle.up()
-		-- s=s+1
-	-- else
-		-- break
-	-- end
--- end
--- while not turtle.detectDown() do
-	-- turtle.down()
--- end
+while true do
+	su,muzhuang=turtle.inspectUp()
+	if muzhuang.name==biaoji.name then
+		turtle.digUp()
+		turtle.up()
+		s=s+1
+	else
+		break
+	end
+end
+while not turtle.detectDown() do
+	turtle.down()
+end
 print("high= ",s)
 
   
